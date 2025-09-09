7f66e1b9645303e87bf8d1c89464a331d3aba2ea
Macos <macos@Macoss-MacBook-Pro.local>
2025-09-09 00:51:18 -0700

    Impact Section

---
+++ script/index.js
@@ -0,0 +1,25 @@
+const loadLessons = () => {
+    fetch('https://openapi.programming-hero.com/api/categories') //Promise of response
+        .then(res => res.json()) //Promise of json data
+        .then((json) => displayLessons(json.data));
+};
+const displayLessons = (lessons) => {
+//    1. get the container & empty
+const levelContainer = document.getElementById("level-container");
+levelContainer.innerHTML = "";
+//    2. get into evey lessons
+for(let lesson of lessons){ 
+//        3. create Element
+const btnDiv = document.createElement("div");
+btnDiv.innerHTML = `<button class="btn btn-outline btn-primary ">
+                      <i class="fa-solid fa-book-open"></i>
+                      Plant a Tree</button>
+                      `;
+
+//        4. append into container
+
+levelContainer.append(btnDiv);
+}
+
+};
+loadLessons();
